632c7008b27ac2b89c6a7890c9ec5c4aef1e77eb
Lewis <lev.bazhkov@aisa.ru>
2024-10-09 01:29:41 +0300

    initial commit

---
+++ Wiki_Scanner/Wiki_Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wiki_Scanner
+//
+//  Created by Lewis on 09.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
